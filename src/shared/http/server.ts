7ca55f0f12c3c11829a0df1c3bb21f140a4e8f5c
Rodrigo <rodsussumu@gmail.com>
2022-01-08 00:06:15 -0300

    add celebrate validations

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,9 +1,11 @@
-import express, { NextFunction, Request, Response } from 'express';
 import 'reflect-metadata';
+import 'express-async-error';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
+import { errors } from 'celebrate';
 
 const app = express();
 
@@ -11,6 +13,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
