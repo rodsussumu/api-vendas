a2e79041930141629e364234c6c493687e20dc85
Rodrigo <rodsussumu@gmail.com>
2022-07-02 02:13:15 -0300

    add update profile

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,5 +1,6 @@
 import productsRouter from '@modules/products/routes/products.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
+import profileRouter from '@modules/users/routes/profile.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
 import usersRouter from '@modules/users/routes/users.routes';
 import { Router } from 'express';
@@ -13,5 +14,7 @@ routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 
 routes.use('/password', passwordRouter);
+
+routes.use('/profile', profileRouter);
 
 export default routes;
